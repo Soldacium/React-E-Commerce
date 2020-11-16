4752d36b05c273273b15bf506eb268f895f1b373
Soldacium <laurentyfenix@gmail.com>
2020-11-16 19:36:37 +0100

    shop setup & first routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { HomePage } from './homepage.component'
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop' component={ShopPage}/>
+
     </div>
   );
 }
