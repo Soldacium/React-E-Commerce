5a3fb6224a0b577d0115c36c47f4a0c885a07b0f
Soldacium <laurentyfenix@gmail.com>
2020-11-16 17:06:46 +0100

    initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+export const HomePage = () => {
+    return(
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HEADER</h1>
+                        <span className='subtitle'>SPAN</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HEADER</h1>
+                        <span className='subtitle'>SPAN</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HEADER</h1>
+                        <span className='subtitle'>SPAN</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HEADER</h1>
+                        <span className='subtitle'>SPAN</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HEADER</h1>
+                        <span className='subtitle'>SPAN</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
