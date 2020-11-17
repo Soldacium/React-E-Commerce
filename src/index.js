4c7b10385b056f84d46d70e44b3def9f3dc619dd
Soldacium <laurentyfenix@gmail.com>
2020-11-17 23:06:19 +0100

    added persistance of cart when closing window/tab

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
 
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './redux/store'
+import { store,persistor} from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+        
       </BrowserRouter>      
     </Provider>
 
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'; //for window storage, another for session storage
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] //persist only cart, this is array of reducers to persist
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
