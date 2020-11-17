47750e1fd757d54ad43da718e3f42a5bd02cbc2e
Soldacium <laurentyfenix@gmail.com>
2020-11-18 00:30:07 +0100

    more redux work and improvements; moved shop_data and directory data, created thier selectors and implemented to components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,18 @@
 import React from 'react'
-import SHOP_DATA from './shop.data'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
 
-        this.state ={
-            collections: SHOP_DATA
-        }
-    }
+const ShopPage = () =>  {
 
-    render() {
-        const {collections} = this.state
-        return(
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
+    return(
+        <div className='shop-page'>
+            <CollectionsOverview/>
+        </div>
+    )
 }
+
+
+
 
 export default ShopPage
